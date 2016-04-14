86f154793cd1edc7b74deca17b2e7321dcd2d56e
dailey.yet <dailey.yet@outlook.com>
2016-04-14 23:53:17 +0800

    tochatserver

---
+++ webserver/src/main/java/cc/tochat/webserver/EchoServer.java
@@ -0,0 +1,13 @@
+package cc.tochat.webserver;
+
+import javax.websocket.OnMessage;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/echo")
+public class EchoServer {
+
+	@OnMessage
+	public String echo(String incomingMessage){
+		return "I got this ("+incomingMessage+") so I am sending it back !";
+	}
+}
